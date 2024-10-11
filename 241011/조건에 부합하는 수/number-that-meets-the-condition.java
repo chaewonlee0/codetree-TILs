c17d7bee74699d5299c27435e7049611eb457647
SynergyCoder <lcw2094@gmail.com>
2024-10-11 11:44:28 +0900

    Title: 조건에 부합하는 수, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241011/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int n = Integer.parseInt(st.nextToken());
+
+		int prod = 1, cnt =0;
+
+		for (int i = 1; i <= n; i++) {
+
+
+			if ((i % 2 ==0 && i%4!=0) || i / 8==2 || i / 8 ==4 || i / 8 ==6 ||  i / 8 == 8 || i / 8 ==0 || i % 7 <4 ) {
+		
+				continue;
+			}
+			
+			System.out.print(i+ " ");
+		}
+		
+		
+	}
+}
