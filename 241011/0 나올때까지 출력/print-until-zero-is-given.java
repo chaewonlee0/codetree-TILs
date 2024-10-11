00a1ac60ac9241336c6ac8c93b98a0fae8d85a3d
SynergyCoder <lcw2094@gmail.com>
2024-10-11 15:54:11 +0900

    Title: 0 나올때까지 출력, Time: 80ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241011/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			if (n == 0) {
+				break;
+			}
+			System.out.println(n);
+
+		}
+
+	}
+}
