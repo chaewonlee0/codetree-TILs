6216827b79f0df90de00c74b0dfbf83920cd1f16
SynergyCoder <lcw2094@gmail.com>
2024-10-11 10:52:53 +0900

    Title: a의 b승, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241011/a의 b승/a-to-the-power-of-b.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int prod = 1, cnt =0;
+		double avg ;
+
+		for (int i = 1; i <= b; i++) {
+				prod *= a;
+
+		System.out.println(prod);
+	}
+}
