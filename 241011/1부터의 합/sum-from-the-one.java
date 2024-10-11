b05461acee369d88ad575618e682fb6942250f68
SynergyCoder <lcw2094@gmail.com>
2024-10-11 11:55:08 +0900

    Title: 1부터의 합, Time: 80ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241011/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int n = Integer.parseInt(st.nextToken());
+
+		int sum = 1, cnt =0;
+
+
+		for (int i = 1; i <= 100; i++) {
+
+			sum += i;
+
+			if (sum>=n) {
+
+				System.out.print(i);
+				break;
+			}
+			
+		}
+		
+	}
+}
