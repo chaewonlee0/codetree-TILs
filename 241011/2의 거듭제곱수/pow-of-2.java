c67262a0a0e604df31a0db4a55465e05f3be3ce1
SynergyCoder <lcw2094@gmail.com>
2024-10-11 17:23:20 +0900

    Title: 2의 거듭제곱수, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241011/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		int cnt = 0;
+		double avg = 0;
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		while (true) {
+
+			if (a % 2 == 0) {
+				a /= 2;
+				cnt++;
+			} 
+			if (a == 1) {
+				System.out.println(cnt);
+				break;
+			}
+
+		}
+
+	}
+}
