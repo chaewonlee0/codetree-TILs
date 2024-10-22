94322ed624634a0c0e63f4386674e71f82580378
SynergyCoder <lcw2094@gmail.com>
2024-10-23 00:44:17 +0900

    Title: 이상한 수열, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241023/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		System.out.println(cal(n));
+
+	}
+
+	static int cal(int n) {
+
+		if (n == 1) {
+			return 1;
+		}
+		if (n == 2) {
+			return 2;
+		}
+		return cal(n/ 3) + cal(n - 1);
+
+	}
+}
