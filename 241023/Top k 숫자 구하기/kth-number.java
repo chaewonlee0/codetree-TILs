a8cb9801fd1ef99c8a16cd872d42dff30ec30d64
SynergyCoder <lcw2094@gmail.com>
2024-10-23 16:56:15 +0900

    Title: Top k 숫자 구하기, Time: 103ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241023/Top k 숫자 구하기/kth-number.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.util.Arrays;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		
+		int k = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(bf.readLine());
+		
+		int [] arr = new int [n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		System.out.println(arr[k-1]);
+
+	}
+}
