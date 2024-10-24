6a9a73f19aaa17e8bc8081bfa56244a657a826e8
SynergyCoder <lcw2094@gmail.com>
2024-10-24 20:21:13 +0900

    Title: k번째로 신기한 문자열, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241024/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int arr[];
+	static int sum, n, cnt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		int k = Integer.parseInt(st.nextToken());
+
+		String T = st.nextToken();
+
+		String[] arr = new String[n];
+
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(bf.readLine());
+			String tmp = st.nextToken();
+			if(T.length()> tmp.length()) {
+				continue;
+			}
+			
+			if (tmp.substring(0, T.length()).equals(T)) {
+				arr[cnt++] = tmp;
+			}
+		}
+		arr = Arrays.copyOf(arr, cnt);
+
+		Arrays.sort(arr);
+
+		System.out.println(arr[k - 1]);
+
+	}
+}
